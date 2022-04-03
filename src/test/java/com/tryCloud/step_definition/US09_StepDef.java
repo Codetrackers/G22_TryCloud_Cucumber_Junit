f3fd1ee6bedfe045eb322cc08f2e20d6a21590e3
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-03 10:47:49 -0400

    US09 Step Def

--- src/test/java/com/tryCloud/step_definition/US09_StepDef.java
+++ src/test/java/com/tryCloud/step_definition/US09_StepDef.java
@@ -1,9 +1,15 @@
 package com.tryCloud.step_definition;
 
+import com.tryCloud.page.FilePage;
+import com.tryCloud.page.LogInPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class US09_StepDef {
+
+    LogInPage login = new LogInPage();
+
+    FilePage file = new FilePage();
 
     @When("user click action-icon  from any file on the page")
     public void user_click_action_icon_from_any_file_on_the_page() {
