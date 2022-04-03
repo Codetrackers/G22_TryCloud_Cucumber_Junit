6aa8d819aeef9f29d717e4ca2fb472d6908f4380
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-03 11:10:10 -0400

    US09 Comment Page

--- src/test/java/com/tryCloud/page/CommentPage.java
+++ src/test/java/com/tryCloud/page/CommentPage.java
@@ -1,7 +1,22 @@
 package com.tryCloud.page;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class CommentPage extends BasePage{
 
+    @FindBy (xpath = "//a/span[.='Actions']")
+    public List<WebElement> actionIcon;
 
+    @FindBy (xpath = "//a/span[.='Details']")
+    public WebElement detailsIcon;
+
+    @FindBy (xpath = "//a[@id='commentsTabView']")
+    public WebElement commentBtn;
+
+    @FindBy (xpath = "//section//div[@class='message']")
+    public WebElement commentBox;
 
 }
